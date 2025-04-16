58240b1e00d9165b77e38327dd840557d32d8ff7
Adeshola Shokunbi <sulazo@yahoo.com>
2025-04-16 10:57:10 -0400

    removed code from main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 #terraform code goes here
-he Lorem ipsum text is derived from sections 1.10.32 and 1.10.33 of Cicero's De finibus bonorum et malorum.[6][7] The physical source may have been the 1914 Loeb Classical Library edition of De finibus, where the Latin text, presented on the left-hand (even) pages, breaks off on page 34 with "Neque porro quisquam est qui do-" and continues on page 36 with "lorem ipsum ...", suggesting that the galley type of that page was mixed up to make the dummy text seen today.[1]
-
-The discovery of the text's origin is attributed to Richard McClintock, a Latin scholar at Hampden–Sydney College. McClintock connected Lorem ipsum to Cicero's writing sometime before 1982 while searching for instances of the Latin word consectetur, which was rarely used in classical literature.[2] McClintock first published his discovery in a 1994 letter to the editor of Before & After magazine,[8] contesting the editor's earlier claim that Lorem ipsum held no meaning.[2]
-
-The relevant section of Cicero as printed in the source is reproduced below with fragments used in L
+resource "aws_vpc"{
+    
+    }
